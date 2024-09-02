9b848e8e28fa155cfa97871444d7059df09690cd
itaynvn-runai <165032271+itaynvn-runai@users.noreply.github.com>
2024-09-02 16:12:29 +0300

    Update cli_downloader.sh

--- cli_downloader.sh
+++ cli_downloader.sh
@@ -11,13 +11,14 @@ echo "Downloading binaries for $DIST"
 
 if kubectl get ns runai > /dev/null 2>&1; then
     RUNAI_CLUSTER_URL=$(kubectl -n runai get ingress researcher-service-ingress -o jsonpath='{.spec.rules[0].host}')
-    RUNAI_CLUSTER_VERSION=$(kubectl -n runai get deploy -o wide | tail -n 1 | awk '{ print $7 }' | awk -F: '{print $2}' | tr -d '.')
-    CLI_BINARY_PATH="$HOME/runai-$RUNAI_CLUSTER_VERSION"
-    wget --quiet https://$RUNAI_CLUSTER_URL/cli/$DIST -O $CLI_BINARY_PATH
+    echo "Run:AI cluster URL: $RUNAI_CLUSTER_URL"
+    RUNAI_CLUSTER_VERSION=$(kubectl -n runai get deploy -o wide | tail -n 1 | awk '{ print $7 }' | awk -F: '{print $2}')
+    echo "Run:AI cluster version: $RUNAI_CLUSTER_VERSION"
+    CLI_BINARY_PATH="$HOME/Downloads/runai-$(echo $RUNAI_CLUSTER_VERSION | tr -d '.')"
+    wget --quiet --content-disposition https://$RUNAI_CLUSTER_URL/cli/$DIST -O $CLI_BINARY_PATH
     chmod +x $CLI_BINARY_PATH
-    alias ra=$CLI_BINARY_PATH
-    r version
-    echo "CLI binary saved to '$CLI_BINARY_PATH', use 'r' alias to execute"
+    echo "CLI binary saved to '$CLI_BINARY_PATH', assign it to the 'r' alias by running:"
+    echo "alias r=$CLI_BINARY_PATH"
 else
     echo "Namespace 'runai' does not exist."
 fi
@@ -26,13 +27,14 @@ fi
 
 if kubectl get ns runai-backend > /dev/null 2>&1; then
     RUNAI_CTRL_PLANE_URL=$(kubectl -n runai-backend get ingress runai-backend-ingress -o jsonpath='{.spec.rules[0].host}')
+    echo "Run:AI control plane URL: $RUNAI_CTRL_PLANE_URL"
     RUNAI_CTRL_PLANE_VERSION=$(kubectl -n runai-backend get deploy -o wide | tail -n 1 | awk '{ print $7 }' | awk -F: '{print $2}' | tr -d '.')
-    ADMIN_CLI_BINARY_PATH="$HOME/runai-adm-$RUNAI_CTRL_PLANE_VERSION"
+    echo "Run:AI control plane version: $RUNAI_CTRL_PLANE_VERSION"
+    ADMIN_CLI_BINARY_PATH="$HOME/runai-adm-$(echo $RUNAI_CTRL_PLANE_VERSION | tr -d '.')"
     wget --quiet https://$RUNAI_CTRL_PLANE_URL/v1/k8s/admin-cli/$DIST -O $ADMIN_CLI_BINARY_PATH
     chmod +x $ADMIN_CLI_BINARY_PATH
-    alias ra=$ADMIN_CLI_BINARY_PATH
-    ra version
-    echo "admin CLI binary saved to '$ADMIN_CLI_BINARY_PATH', use 'ra' alias to execute"
+    echo "admin CLI binary saved to '$ADMIN_CLI_BINARY_PATH', assign it to the 'ra' alias by running:"
+    echo "alias ra=$ADMIN_CLI_BINARY_PATH"
 else
     echo "Namespace 'runai-backend' does not exist."
 fi
