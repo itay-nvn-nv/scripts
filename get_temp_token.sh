af6b4799051b236ba5c731087275647c7def1c66
itaynvn-runai <itay.anavian@run.ai>
2024-10-10 12:50:38 +0300

    added debug mode to script

--- get_temp_token.sh
+++ get_temp_token.sh
@@ -2,7 +2,22 @@
 
 # set variables and functions
 
-# Check if exactly 4 parameters are provided
+DEBUG=false  # Default is off
+
+# Check if the script was called with the --debug flag
+if [ "$1" == "--debug" ]; then
+  DEBUG=true
+  shift  # Remove --debug from arguments
+fi
+
+# Debugging helper function
+debug() {
+  if [ "$DEBUG" = true ]; then
+    echo "DEBUG::: $1"
+  fi
+}
+
+# Check if exactly 5 parameters are provided
 if [ "$#" -ne 5 ]; then
   echo "Usage: $0 <username> <password> <CTRL_PLANE_URL> <KEYCLOAK_URL> <KEYCLOAK_REALM>"
   exit 1
@@ -29,14 +44,16 @@ password=$2
 CTRL_PLANE_URL=$3
 KEYCLOAK_URL=$4
 KEYCLOAK_REALM=$5
+client_id="runai-admin-ui" # fixed for runai default client, can be changed: auth/admin/master/console/#/$KEYCLOAK_REALM/clients
 
 # Printing the variables
-echo "### Input values:"
-echo "Username: $username"
-echo "Password: $password"
-echo "Control Plane URL: $CTRL_PLANE_URL"
-echo "Keycloak URL: $KEYCLOAK_URL"
-echo "Keycloak realm: $KEYCLOAK_REALM"
+debug "### Input values:"
+debug "Username: $username"
+debug "Password: $password"
+debug "Control Plane URL: $CTRL_PLANE_URL"
+debug "Keycloak URL: $KEYCLOAK_URL"
+debug "Keycloak realm: $KEYCLOAK_REALM"
+debug "Keycloak client ID: $client_id"
 echo
 
 # encoding vars
@@ -47,43 +64,43 @@ password_encoded=$(urlencode $password)
 redirect_uri_encoded=$(urlencode $REDIRECT_URI_DECODED)
 iss_encoded=$(urlencode $ISS_DECODED)
 
-echo "### Generated URL-encoded values:"
-echo "Username: $username_encoded"
-echo "Password: $password_encoded"
-echo "redirect URI: $redirect_uri_encoded"
-echo "iss: $iss_encoded"
+debug "### Generated URL-encoded values:"
+debug "Username: $username_encoded"
+debug "Password: $password_encoded"
+debug "redirect URI: $redirect_uri_encoded"
+debug "iss: $iss_encoded"
 echo
 
 # get login page
 echo "### Step 1: get login page"
 echo
-response=$(curl -s -i curl "$KEYCLOAK_URL/auth/realms/$KEYCLOAK_REALM/protocol/openid-connect/auth?response_type=code&connection=$KEYCLOAK_REALM&client_id=runai-admin-ui&redirect_uri=$redirect_uri_encoded&scope=openid+email+profile+offline_access")
+response=$(curl -s -i curl "$KEYCLOAK_URL/auth/realms/$KEYCLOAK_REALM/protocol/openid-connect/auth?response_type=code&connection=$KEYCLOAK_REALM&client_id=$client_id&redirect_uri=$redirect_uri_encoded&scope=openid+email+profile+offline_access")
 
 AUTH_SESSION_ID=$(echo "$response" | grep "AUTH_SESSION_ID=" | awk -F'AUTH_SESSION_ID=' '{print substr($2, 1, 53)}')
 AUTH_SESSION_ID_LEGACY=$(echo "$response" | grep "AUTH_SESSION_ID_LEGACY=" | awk -F'AUTH_SESSION_ID_LEGACY=' '{print substr($2, 1, 53)}')
 KC_RESTART=$(echo "$response" | grep "KC_RESTART=" | awk -F'KC_RESTART=' '{print substr($2, 1, 991)}')
 
-echo "### Response cookies:"
-echo "AUTH_SESSION_ID: ${AUTH_SESSION_ID}"
-echo "AUTH_SESSION_ID_LEGACY: ${AUTH_SESSION_ID_LEGACY}"
-echo "KC_RESTART: ${KC_RESTART}"
+debug "### Response cookies:"
+debug "AUTH_SESSION_ID: ${AUTH_SESSION_ID}"
+debug "AUTH_SESSION_ID_LEGACY: ${AUTH_SESSION_ID_LEGACY}"
+debug "KC_RESTART: ${KC_RESTART}"
 echo
 
 session_code=$(echo $response | grep "login-actions/authenticate" | awk -F'session_code=' '{print substr($2, 1, 43)}')
 execution=$(echo $response | grep "login-actions/authenticate" | awk -F'execution=' '{print substr($2, 1, 36)}')
 tab_id=$(echo $response | grep "login-actions/authenticate" | awk -F'tab_id=' '{print substr($2, 1, 11)}')
 
-echo "### Response URL params:"
-echo "session_code: ${session_code}"
-echo "execution: ${execution}"
-echo "tab_id: ${tab_id}"
+debug "### Response URL params:"
+debug "session_code: ${session_code}"
+debug "execution: ${execution}"
+debug "tab_id: ${tab_id}"
 echo
 
 # perform login
 echo "### Step 2: perform login"
 echo
 
-login_response=$(curl -v "$KEYCLOAK_URL/auth/realms/$KEYCLOAK_REALM/login-actions/authenticate?session_code=$session_code&execution=$execution&client_id=runai-admin-ui&tab_id=$tab_id" \
+login_response=$(curl -v "$KEYCLOAK_URL/auth/realms/$KEYCLOAK_REALM/login-actions/authenticate?session_code=$session_code&execution=$execution&client_id=$client_id&tab_id=$tab_id" \
   -H "content-type: application/x-www-form-urlencoded" \
   -H "cookie: AUTH_SESSION_ID=$AUTH_SESSION_ID; AUTH_SESSION_ID_LEGACY=$AUTH_SESSION_ID_LEGACY; KC_RESTART=$KC_RESTART" \
   --data-raw "username=$username_encoded&password=$password_encoded&credentialId=" 2>&1)
@@ -91,9 +108,9 @@ login_response=$(curl -v "$KEYCLOAK_URL/auth/realms/$KEYCLOAK_REALM/login-action
 session_state=$(echo -e $login_response | grep "location: " | awk -F'session_state=' '{print substr($2, 1, 36)}')
 code=$(echo -e $login_response | grep "location: " | awk -F'&code=' '{print substr($2, 1, 110)}')
 
-echo "### Response URL params:"
-echo "session_state: ${session_state}"
-echo "code: ${code}"
+debug "### Response URL params:"
+debug "session_state: ${session_state}"
+debug "code: ${code}"
 echo
 
 # get token
@@ -112,6 +129,23 @@ token_response=$(curl -s "$CTRL_PLANE_URL/api/v1/token" \
 
 id_token=$(echo $token_response | jq -r '.idToken')
 echo "### the idToken: $id_token"
+echo
+
+# show decoded token
+IFS='.' read -r header payload signature <<< "$id_token"
+
+# Decode the header (Base64 URL)
+decoded_header=$(echo "$header" | base64 --decode 2>/dev/null | jq .)
+
+# Decode the payload (Base64 URL)
+decoded_payload=$(echo "$payload" | base64 --decode 2>/dev/null | jq .)
+
+# Print the decoded results
+debug "Decoded Header:"
+debug "$decoded_header"
+echo
+debug "Decoded Payload:"
+debug "$decoded_payload"
 echo
 
 echo "### Step 4: test token"
