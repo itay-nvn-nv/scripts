c9384de55218d19687cce7fdd5c04fa9eedd200d
itaynvn-runai <itay.anavian@run.ai>
2024-12-23 11:56:40 +0200

    some updates

--- cli_downloader.sh
+++ cli_downloader.sh
@@ -30,15 +30,14 @@ if kubectl get ns runai > /dev/null 2>&1; then
     RUNAI_CLUSTER_URL=$(kubectl -n runai get ingress researcher-service-ingress -o jsonpath='{.spec.rules[0].host}')
     if [ -z "$RUNAI_CLUSTER_URL" ]; then
         RUNAI_CLUSTER_URL=$RUNAI_CTRL_PLANE_URL
-    else
-        echo "Run:AI cluster URL: $RUNAI_CLUSTER_URL"
-        RUNAI_CLUSTER_VERSION=$(kubectl -n runai get deploy -o wide | tail -n 1 | awk '{ print $7 }' | awk -F: '{print $2}')
-        echo "Run:AI cluster version: $RUNAI_CLUSTER_VERSION"
-        CLI_BINARY_PATH="$HOME/runai-$(echo $RUNAI_CLUSTER_VERSION | tr -d '.')"
-        wget https://$RUNAI_CLUSTER_URL/cli/$DIST -O $CLI_BINARY_PATH
-        chmod +x $CLI_BINARY_PATH
-        echo "CLI binary saved to '$CLI_BINARY_PATH', assign it to the 'r' alias by running:"
-        echo "alias r=$CLI_BINARY_PATH"
+    echo "Run:AI cluster URL: $RUNAI_CLUSTER_URL"
+    RUNAI_CLUSTER_VERSION=$(kubectl -n runai get deploy -o wide | tail -n 1 | awk '{ print $7 }' | awk -F: '{print $2}')
+    echo "Run:AI cluster version: $RUNAI_CLUSTER_VERSION"
+    CLI_BINARY_PATH="$HOME/runai-$(echo $RUNAI_CLUSTER_VERSION | tr -d '.')"
+    wget https://$RUNAI_CLUSTER_URL/cli/$DIST -O $CLI_BINARY_PATH
+    chmod +x $CLI_BINARY_PATH
+    echo "CLI binary saved to '$CLI_BINARY_PATH', assign it to the 'r' alias by running:"
+    echo "alias r=$CLI_BINARY_PATH"
     fi
 else
     echo "Namespace 'runai' does not exist."
--- log_saver.sh
+++ log_saver.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# Set the namespace to collect logs from
-NAMESPACE="runai-backend"
+# usage:
+# bash log_saver.sh mycoolnamespace
+# output:
+# ./mycoolnamespace-logs/
 
-# Create a directory to store logs
-LOG_DIR=$(pwd)
+NAMESPACE=$1
+LOG_DIR="./$NAMESPACE-logs"
+mkdir $LOG_DIR
 
-# Get the list of all pods in the namespace
 PODS=$(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
 
-# Loop through each pod
 for POD in $PODS; do
-  # Get containers in the pod
   CONTAINERS=$(kubectl get pod $POD -n $NAMESPACE -o jsonpath='{.spec.containers[*].name}')
   
   for CONTAINER in $CONTAINERS; do
-    # Save logs to a file named after the pod and container
     LOG_FILE="$LOG_DIR/${POD}_${CONTAINER}.log"
     
-    # Collect logs
     echo "Collecting logs for Pod: $POD, Container: $CONTAINER"
     kubectl logs --timestamps $POD -c $CONTAINER -n $NAMESPACE > "$LOG_FILE"
   done
